c1f480ad885aa56e383ec96fad5ed2b04cc77aaf
Vinay55-creator <vinaypawaskar600@gmail.com>
2023-12-29 03:12:26 +0530

    Updates

---
+++ precache-manifest.0ed3054a1304279022c81c94e32ee212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef33a5f31dc3e68420528c4394ab58e",
+    "url": "/Textutils/index.html"
+  },
+  {
+    "revision": "37859d1e583117688d68",
+    "url": "/Textutils/static/css/main.3adb503d.chunk.css"
+  },
+  {
+    "revision": "8bca827d70f3d8b1f1bd",
+    "url": "/Textutils/static/js/2.2edda431.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Textutils/static/js/2.2edda431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82594277ffdbbbbe6c96",
+    "url": "/Textutils/static/js/3.f00e9902.chunk.js"
+  },
+  {
+    "revision": "37859d1e583117688d68",
+    "url": "/Textutils/static/js/main.e054b93f.chunk.js"
+  },
+  {
+    "revision": "3282e05f18aa73b703f7",
+    "url": "/Textutils/static/js/runtime-main.1bf7e713.js"
+  }
+]);
